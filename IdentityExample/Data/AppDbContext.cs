05d194ab33713e17c0cf4e3209f7155dbca0c401
LillianLin <a888gogogo@yahoo.com.tw>
2021-04-18 16:39:18 +0800

    DI DbContext service

--- IdentityExample/Data/AppDbContext.cs
+++ IdentityExample/Data/AppDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityExample.Data
 {
     public class AppDbContext : IdentityDbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> config):base(config)
+        {
+            
+        }
     }
 }
